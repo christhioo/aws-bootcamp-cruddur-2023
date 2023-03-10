d9e4ceb0bdcfb8115df074284dc69ea29029b106
christhioo <christhioo@gmail.com>
2023-03-10 17:24:25 +0000

    implement jwt-token in backend-flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # HoneyComb -------------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -71,11 +73,18 @@ RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
+cognito_jwt_token = CognitoJwtToken(
+   user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+   user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+   region=os.getenv("AWS_DEFAULT_REGION")
+)
+
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -144,10 +153,27 @@ def data_create_message():
     return model['data'], 200
   return
 
+# @app.route("/api/activities/home", methods=['GET'])
+# def data_home():
+  # data = HomeActivities.run(logger=LOGGER)
+  # data = HomeActivities.run()
+  # return data, 200
+
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  # data = HomeActivities.run(logger=LOGGER)
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenticated request
+    app.logger.debug("authenticated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenticated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
