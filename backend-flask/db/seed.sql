432d9cba4549f2a2ccd34909436d56928dfd630d
christhioo <christhioo@gmail.com>
2023-03-17 13:46:32 +0000

    Bash scripting for common database actions

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Christhio Brown', 'christhio' ,'MOCK'),
+  ('Chris Bayko', 'bayko' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'christhio' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
